3866480e9dec0ead4081045a28cf9115e5d84f07
Erin Keller <aestheticartist@gmail.com>
2023-07-11 09:24:58 -0400

    installed additional dependencies in the package files, updated reducer, added store

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,9 @@ import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
